b126412a6e577b3be46f726ae2b13203a1f5c341
FuriousPws002 <1938485828@qq.com>
2024-08-04 23:27:18 +0800

    jquery

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,9 @@
 // Import our custom CSS
 import '../css/index.css'
+
+import $ from 'jquery';
+window.$ = $;
+
+$(".card").click(function () {
+    window.location.href = "/profile.html";
+})
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { resolve } from 'path'
+import inject from "@rollup/plugin-inject";
 
 export default {
   root: resolve(__dirname, 'src'),
+  plugins: [
+    inject({
+      $: 'jquery',
+      jQuery: 'jquery',
+    })
+  ],
   build: {
     outDir: '../dist'
   },
